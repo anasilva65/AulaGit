613845a821902e37db3c37d9137b9bb5d3410155
Anna Silva <annadasilva89@hotmail.com>
2020-06-12 18:00:46 -0300

    initial commit

---
+++ logica3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace logica3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] frutas = new string[3] { "Laranja", "Pera", "Uva" };
+            Console.WriteLine("A fruta numero dois:" +frutas[1]);
+            Console.ReadLine();
+
+        }
+    }
+}
